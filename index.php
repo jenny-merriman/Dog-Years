5b2f41e1126b0eda0bfb45988bb282440ba249b4
jenny-merriman <jennynicole1@hotmail.co.uk>
2020-08-12 07:22:01 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+//My age//
+const myAge = 38;
+
+//first two years//
+let earlyYears = 2;
+
+//Early years age in dog's years//
+earlyYears = earlyYears * 10.5;
+
+//number of years following the first 2 years//
+let laterYears = myAge - 2;
+
+//later years age in dogs years//
+laterYears *=4;
+
+console.log(`${earlyYears} ${laterYears}`)
+
+//early years + later years//
+myAgeInDogYears = earlyYears + laterYears
+
+//my name in lowercase letters//
+let myName = 'Jenny Merriman'.toLowerCase()
+
+//print//
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
